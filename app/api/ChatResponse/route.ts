439f62b809973179a52e2b559558ed061950a147
snowman <snowmandot0520@gmail.com>
2024-05-17 09:09:22 +0900

    feat: add server components

---
+++ app/api/ChatResponse/route.ts
@@ -0,0 +1,40 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextRequest, NextResponse } from 'next/server'
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
+export async function POST(request: NextRequest) {
+    try {
+        const { message, language } = await request.json();
+        // Add a system message to instruct the model to focus on providing SQL queries
+        const newMessages = [
+            {role: 'system', content: `You have to answer in ${language}`},
+            message
+        ];
+
+        // Ask OpenAI for a streaming chat completion given the prompt
+        const chatCompletion = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: newMessages,
+        });
+        const response = {
+            answer: chatCompletion?.choices[0].message.content?.trim(),
+        };
+
+        return NextResponse.json(response);
+    } catch (error) {
+        return NextResponse.error();
+    }
+    
+
+    // // Convert the response into a friendly text-stream
+    // const stream = OpenAIStream(response);
+    // // Respond with the stream
+    // return new StreamingTextResponse(stream);
+}
---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Commune Speech to Speech",
+  description:
+    "Making the Commune's best AI chat models available to everyone.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
